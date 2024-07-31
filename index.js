ab6e989c79f2653261c0b27c1d4dd93bf0d41a93
Rukosuev Alexander <a.rukosuev@samolet.ru>
2024-07-31 15:01:49 +0300

    fs, stream

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
 
 const fs = require('fs')
-const path = require('path')
+const readerStream = fs.createReadStream('package.json')
+
+let data
+readerStream
+.setEncoding('UTF-8')
+.on('data', (chank) => {
+    data += chank
+})
+.on('end', () => {
+    console.log('end', data)
+})
+
+
+const content = 'content\n'
+
+const writeStream = fs.createWriteStream('output.txt')
+writeStream.write(content, 'UTF-8')
+writeStream.end()
+
+writeStream.on('finish', () => {
+    console.log('finish')
+})
+
+writeStream.on('close', () => {
+    console.log('close')
+})
+
+writeStream.on('error', () => {
+    console.log('error')
+})
+
+let readerStream1 = fs.createReadStream('package.json')
+let writeStream1 = fs.createWriteStream('output.txt')
+
+readerStream1.pipe(writeStream1)
 
 // console.log(path.parse(__filename))
 
 // console.log(path.join(__filename, 'test', '..', '//demo.txt'))
 
-const dir = path.join(__dirname, 'demo')
+// const dir = path.join(__dirname, 'demo')
 
 // fs.mkdir(dir, (err) => {
 //     if (err) throw Error(err)
 //     console.log('ok')
 // })
 
-const file = path.join(__dirname, 'demo', 'new.txt')
+// const file = path.join(__dirname, 'demo', 'new.txt')
 
-const content = 'content2\n'
+// const content = 'content2\n'
 
 // fs.writeFile(file, content, (err) => {
 //     if (err) throw Error(err)
@@ -27,7 +61,7 @@ const content = 'content2\n'
 //     console.log('ok')
 // })
 
-fs.readFile(file, 'utf-8', (err, data) => {
-    if (err) throw Error(err)
-    console.log(data)
-})
+// fs.readFile(file, 'utf-8', (err, data) => {
+//     if (err) throw Error(err)
+//     console.log(data)
+// })
