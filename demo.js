0a9a975b999aac8e7736ff73fa92c8a358b72506
Rukosuev Alexander <a.rukosuev@samolet.ru>
2024-07-31 13:08:05 +0300

    path, fs

---
+++ demo.js
@@ -0,0 +1,9 @@
+/*
+module.exports = {
+    'hello': () => 'hello',
+    'demo': () => 'demo',
+}
+*/
+
+module.exports.hello = () => 'hello 1',
+module.exports.demo = () => 'demo 1'
--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers')
 
-const argv = yargs(hideBin(process.argv))
-    .option('params1', {
-        alias: "p1",
-        type: "boolean",
-        description: "params 1 desc"
-    })
-    .option('params2', {
-        alias: "p2",
-        type: "string",
-        description: "params 2 desc",
-        default: "def params 2"
-    })
-    .argv
+const fs = require('fs')
+const path = require('path')
 
+// console.log(path.parse(__filename))
 
-console.log(argv)
+// console.log(path.join(__filename, 'test', '..', '//demo.txt'))
+
+const dir = path.join(__dirname, 'demo')
+
+// fs.mkdir(dir, (err) => {
+//     if (err) throw Error(err)
+//     console.log('ok')
+// })
+
+const file = path.join(__dirname, 'demo', 'new.txt')
+
+const content = 'content2\n'
+
+// fs.writeFile(file, content, (err) => {
+//     if (err) throw Error(err)
+//     console.log('ok')
+// })
+
+// fs.appendFile(file, content, (err) => {
+//     if (err) throw Error(err)
+//     console.log('ok')
+// })
+
+fs.readFile(file, 'utf-8', (err, data) => {
+    if (err) throw Error(err)
+    console.log(data)
+})
